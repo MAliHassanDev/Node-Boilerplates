8e1ea26c61dd5f455648e7251b787f701417e139
ali <muhammadalihassanoffiial@gmail.com>
2025-02-15 08:23:00 +0500

    refactor: change swagger docs title

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,26 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module.js";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // build swagger open api docs
   const config = new DocumentBuilder()
-    .setTitle("AceBeauty api")
-    .setDescription("Api documentation for AceBeauty club")
+    .setTitle("Nestjs Boilerplate")
+    .setDescription("Api documentation for Example.com")
     .setVersion("1.0")
-    .addTag("aceBeauty")
     .build();
   const swaggerFactory = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("/docs", app, swaggerFactory);
 
+  // set global prefix for all routes
+  app.setGlobalPrefix("/api/v1");
+
   // start the server
   await app.listen(process.env.PORT ?? 3000);
+
+  Logger.log(`Server listening at ${await app.getUrl()}`, "Main");
 }
 void bootstrap();
