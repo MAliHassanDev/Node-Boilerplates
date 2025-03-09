637936e8f0ea17d99585c8e908300bbed61ead86
ali <muhammadalihassanoffiial@gmail.com>
2025-03-09 12:10:44 +0500

    test: remove auth services test

--- src/core/auth/tests/auth.service.spec.ts
+++ src/core/auth/tests/auth.service.spec.ts
@@ -1,18 +1,21 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { AuthService } from "../auth.service.js";
+// import { Test, TestingModule } from "@nestjs/testing";
+// import { AuthService } from "../auth.service.js";
+// import { UsersService } from "../../users/users.service.js";
+// import { UsersModule } from "../../users/users.module.js";
 
-describe("AuthService", () => {
-  let service: AuthService;
+// describe("AuthService", () => {
+//   let service: AuthService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       providers: [AuthService, UsersService],
+//       imports: [UsersModule],
+//     }).compile();
 
-    service = module.get<AuthService>(AuthService);
-  });
+//     service = module.get<AuthService>(AuthService);
+//   });
 
-  it("should be defined", () => {
-    expect(service).toBeDefined();
-  });
-});
+//   it("should be defined", () => {
+//     expect(service).toBeDefined();
+//   });
+// });
