f16559b9ed38fe883e56dee839dd22910b926fec
ali <muhammadalihassanoffiial@gmail.com>
2025-03-09 12:04:52 +0500

    feat: add api tags in ap controller

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service.js";
+import { ApiOkResponse, ApiTags } from "@nestjs/swagger";
 
 @Controller()
+@ApiTags("App")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOkResponse()
   @Get()
   getHello(): string {
     return this.appService.getHello();
