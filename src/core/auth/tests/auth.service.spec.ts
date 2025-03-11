603c2502cbbbc8f745dd049b617691c8e25b54a2
ali <muhammadalihassanofficial@gmail.com>
2025-03-11 08:41:05 +0500

    test: auth service

--- src/core/auth/tests/auth.service.spec.ts
+++ src/core/auth/tests/auth.service.spec.ts
@@ -1,21 +1,27 @@
-// import { Test, TestingModule } from "@nestjs/testing";
-// import { AuthService } from "../auth.service.js";
-// import { UsersService } from "../../users/users.service.js";
-// import { UsersModule } from "../../users/users.module.js";
+import { Test, TestingModule } from "@nestjs/testing";
+import { AuthService } from "../auth.service.js";
+import { UsersService } from "../../users/users.service.js";
 
-// describe("AuthService", () => {
-//   let service: AuthService;
+describe("AuthService", () => {
+  let service: AuthService;
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [AuthService, UsersService],
-//       imports: [UsersModule],
-//     }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: {
+            findOne: vi.fn(),
+          },
+        },
+      ],
+    }).compile();
 
-//     service = module.get<AuthService>(AuthService);
-//   });
+    service = module.get<AuthService>(AuthService);
+  });
 
-//   it("should be defined", () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+});
